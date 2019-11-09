020f8e2ca01962f079322f5d1d1432b869d5af73
linhuaxin <linhuaxin@foxmail.com>
2019-11-09 19:50:55 +0800

    DND...

---
+++ components/BoardSquare/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useDrop } from 'react-dnd'
+import Square from '../Square'
+import Overlay from '../Overlay'
+import types from '../../types'
+import { moveKnight, canMoveKnight } from '../Game'
+
+function BoardSquare({ x, y, children }) {
+  const black = (x + y) % 2 === 1
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: types.KNIGHT,
+    drop: () => moveKnight(x, y),
+    canDrop: () => canMoveKnight(x, y),
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  })
+
+  return (
+    <div
+      ref={drop}
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <Square black={black}>{children}</Square>
+      {isOver && !canDrop && <Overlay color="red" />}
+      {!isOver && canDrop && <Overlay color="yellow" />}
+      {isOver && canDrop && <Overlay color="green" />}
+    </div>
+  )
+}
+
+export default BoardSquare
