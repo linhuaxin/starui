0bbd4f3f353a6f3765a6db953ab91992d6b7c82e
linhuaxin <linhuaxin@foxmail.com>
2019-11-13 22:50:03 +0800

    Tab...

---
+++ components/tab/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+import { Tab } from './style'
+
+class TabComponent extends PureComponent {
+  render() {
+    <Tab>123</Tab>
+  }
+}
+
+export default TabComponent
