171356a6c0a962adc1e7c457bdf5e3741f3df5d5
linhuaxin <linhuaxin@foxmail.com>
2019-11-14 23:09:14 +0800

    Input...

---
+++ components/input/index.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+import { Input } from './style'
+
+class SelectComponent extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <Input>
+      </Input>
+    )
+  }
+}
+
+export default SelectComponent
