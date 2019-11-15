7f4cbeb927629e3d39189293ccfcda414fa292f7
linhuaxin <linhuaxin@foxmail.com>
2019-11-15 22:47:30 +0800

    Pagination...

---
+++ components/pagination/index.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react'
+import { Select } from './style'
+import { CSSTransition } from 'react-transition-group'
+
+class PaginationComponent extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    let { list } = this.props
+
+    return (
+      <Select>
+      </Select>
+    )
+  }
+}
+
+export default PaginationComponent
